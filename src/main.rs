d7db666c4ecde5c7f4b5556badc19156908ee397
Jasper O'Leary <jhaazpr@gmail.com>
2020-02-28 08:53:04 -0800

    bootable image

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static YASS_QUEEN: &[u8] = b"YAASSSSS QUEEN.";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in YASS_QUEEN.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
